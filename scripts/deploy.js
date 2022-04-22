8db8cea18c91696cdbd004250b21e0c80f1978b1
Joseph Dubon <josephdubon@pm.me>
2022-04-22 12:32:28 -0700

    typo: 'Hello World!' -> 'HelloWorld' and added upper case to 'World' on line 12

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,13 +3,13 @@ async function main() {
     // contracts, so HelloWorld here is a factory for instances of our hello world contract.
     // When using the hardhat-ethers plugin ContractFactory and Contract, instances are connected
     // to the first signer (owner) by default.
-    const HelloWorld = await ethers.getContractFactory('Hello World!')
+    const HelloWorld = await ethers.getContractFactory('HelloWorld')
 
     // Calling deploy() on a ContractFactory will start the deployment, and return a Promise that
     // resolves to a Contract object. This is the object that has a method for each of our smart contract functions.
 
     // Start deployment, returning a promise that resolves to a contract object.
-    const hello_world = await HelloWorld.deploy('Hello world!')
+    const hello_world = await HelloWorld.deploy('Hello World!')
     console.log('Contract deployed to address: ', hello_world.address)
 }
 
