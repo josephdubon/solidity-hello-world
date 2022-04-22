d78a09b857618dee712346b6a4cb17ab4b9c35c6
Joseph Dubon <josephdubon@pm.me>
2022-04-22 12:03:02 -0700

    updated imports and module.exports

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+import 'dotenv/config'
+import '@nomiclabs/hardhat-ethers'
+
+const {ALCHEMY_API_URL, METAMASK_PRIVATE_KEY} = process.env
+
 module.exports = {
-  solidity: "0.7.3",
-};
+    solidity: '0.7.3',
+    defaultNetwork: 'ropsten',
+    networks: {
+        hardhat: {},
+        ropsten: {
+            url: ALCHEMY_API_URL,
+            accounts: [`0x#{METAMASK_PRIVATE_KEY}`]
+        }
+    },
+}
